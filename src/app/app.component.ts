f3896d12d498427f133ac1facbf90b3a2ceada88
OussamaAlouat <al317257@uji.es>
2019-12-04 19:29:09 +0100

    Cleaned app component and added first component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'curso-angular8';
+  title = 'Curso de angular 8 avanzado ';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { TiendaComponent } from './components/tienda/tienda.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TiendaComponent,
   ],
   imports: [
     BrowserModule,
