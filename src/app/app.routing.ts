693f77ce323d226e20d912e453052876866caa99
oussamaAlouat <al317257@uji.es>
2020-01-08 16:42:30 +0100

    Added new routes for new components

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,11 +3,21 @@ import { Routes, RouterModule } from '@angular/router';
 
 // Components
 import { TiendaComponent } from './components/tienda/tienda.component';
+import { ParquesComponent } from './components/parques/parques.component';
+import { AnimalsComponent } from './components/animals/animals.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
+import { KeepersComponent } from './components/keepers/keepers.component';
 
 const appRoutes: Routes = [
-  {path: '', component: TiendaComponent},
-  {path: '', redirectTo: 'tienda', pathMatch: 'full'},
-  {path: 'tienda', component: TiendaComponent}
+  {path: '', component: HomeComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'tienda', component: TiendaComponent},
+  {path: 'animales', component: AnimalsComponent},
+  {path: 'contacto', component: ContactComponent},
+  {path: 'cuidadores', component: KeepersComponent},
+  {path: '**', component: HomeComponent},
 ];
 
 export const appRoutingProviders:any[] = [];
