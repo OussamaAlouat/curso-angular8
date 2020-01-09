6380714416f7ca13ebce821eef90409fea313b00
oussamaAlouat <al317257@uji.es>
2020-01-09 09:19:47 +0100

    Change the app name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component, DoCheck, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements DoCheck {
-  title = 'Curso de angular 8 avanzado ';
+  title = 'NGZOO ';
   public emailContacto: string;
 
   ngDoCheck() {
